78d623661f49d55f2163b84a71a88c1b307c049d
Jen Carrigan <jen.d.carrigan@gmail.com>
2018-12-19 10:44:25 -0800

    added driver and login components

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import Nav from './modules/nav.jsx';
 import Footer from './modules/footer.jsx';
 import RequestDonate from './pages/requestdonate.jsx';
+import Driver from './pages/driver.jsx';
+import Auth from './auth/auth.js';
+import Login from './auth/login.js';
+import LoginContext from './auth/context.js';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Nav />
-        <RequestDonate />
+        <LoginContext>
+          <Login />
+          <Auth capability='driver'>
+            <Driver />
+          </Auth>
+        </LoginContext>
         <Footer />
       </React.Fragment>
     );
