053498e7a0bf45beaee817e149db4a410f671f5d
Jen Carrigan <jen.d.carrigan@gmail.com>
2018-12-20 09:49:49 -0800

    added home page test

---
+++ src/__tests__/home.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from '../components/pages/home.jsx';
+import Enzyme, { shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<Home/>', () => {
+  it('is alive at app start', () => {
+    let app = shallow(<Home />);
+    expect (app.find('span').exists()).toBeTruthy();
+  });
+});
