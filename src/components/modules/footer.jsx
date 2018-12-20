0e571df67f3054949a10a9a48e00c837e8308203
Jane Smith <example@email.com>
2018-12-19 17:41:58 -0800

    added content to about page and css

--- src/components/modules/footer.jsx
+++ src/components/modules/footer.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
-
 import styles from './footer.module.scss';
 
 
 function Footer() {
     return (
       <footer className={styles.footer}>
+
         <span className={styles.footerName}>fooDriver</span>
-        <span className={styles.corp}>&copy; fooDrive All Rights reserved</span>
+
+        <span className={styles.rights}>&copy; fooDrive All Rights reserved</span>
+
+        <span className={styles.contact}>Contact</span>
+       
       </footer>
     );
   }
 
 
 export default Footer;
+
+
 
 
 
--- src/components/pages/about.jsx
+++ src/components/pages/about.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
 
+import styles from './about.module.scss';
+
+import hero from '../../assets/hero.jpg';
+
 const About = props => {
   return (
-      <h1>About Foo Driver</h1>
+    <React.Fragment>
+      <div class={styles.container}>
+    
+        <section className={styles.fooDrive}>
+        <h1>foo Driver</h1>
+        <p>Makes it easy to donate and make a request.It facilitates delivering donations to the people who are in need. It creates a platform to identify who is in need and who is willing to help. We bring the distributer to you.</p>
+        <img src='https://via.placeholder.com/300x200' alt='a picture'/>
+        </section>
+
+        <section className={styles.easyDonate}>
+        <h2>Easy Donate</h2>
+        <p>Facilitates delivering donations to the people who are in need. It creates a platform to identify who is in need and who is willing to help. We bring the distributer to you.</p>
+        <img src='https://via.placeholder.com/300x200' alt='a picture'/>
+        </section>
+
+        <section className={styles.easyRequest}>
+        <h2>Easy Request</h2>
+        <p>Facilitates delivering donations to the people who are in need. It creates a platform to identify who is in need and who is willing to help. We bring the distributer to you.</p>
+        <img src='https://via.placeholder.com/300x200' alt='a picture'/>
+        </section>
+
+
+      </div>
+      
+      
+      </React.Fragment>
   )
 }
 
