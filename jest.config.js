b50f6f79bfdb4b047dbd19385a691203d8e1d438
George McCadden III <thethirdone.g@gmail.com>
2018-12-20 16:22:03 -0800

    Wrote a test for each module and page

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  setupFiles: ['<rootDir>/__tests__/shim.js',
-  '<rootDir>/__tests__/setup.js'],
+  setupFiles: ['<rootDir>/src/__tests__/shim.js',
+  '<rootDir>/src/__tests__/setup.js'],
 }
